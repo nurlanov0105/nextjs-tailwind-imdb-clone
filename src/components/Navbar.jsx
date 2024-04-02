37382f7e5c978d53213c56b4b973452951a4d1af
Nursultan <nurlanov0105@gmail.com>
2024-04-02 16:19:03 +0600

    fixed suspense error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
+import { Suspense } from 'react';
 import NavbarItem from './NavbarItem';
 
 const Navbar = () => {
    return (
       <div className='flex dark:bg-gray-600 bg-amber-100 p-4 lg:text-lg justify-center gap-6'>
-         <NavbarItem title='Trending' param='fetchTrending' />
-         <NavbarItem title='Top Rated' param='fetchTopRated' />
+         <Suspense fallback={<h1>Loading...</h1>}>
+            <NavbarItem title='Trending' param='fetchTrending' />
+         </Suspense>
+         <Suspense fallback={<h1>Loading...</h1>}>
+            <NavbarItem title='Top Rated' param='fetchTopRated' />
+         </Suspense>
       </div>
    );
 };
